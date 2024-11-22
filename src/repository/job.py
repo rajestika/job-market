59b8f1a3ef9c47d1e5555c87b2b04f8bf353016f
rajestika <rajestika@gmail.com>
2024-11-22 20:24:23 +0700

    add "query" for every variable name in sql_query.py

--- src/repository/job.py
+++ src/repository/job.py
@@ -2,37 +2,37 @@ from apps.src.main import cursor, connection
 import sql_query
 
 def get_job_list():
-    cursor.execute(sql_query.FETCH_ALL_JOB)
+    cursor.execute(sql_query.FETCH_ALL_JOB_QUERY)
 
     record = cursor.fetchall()
     return record
 
 def get_job(job_name):
-    cursor.execute(sql_query.FETCH_JOB_BY_JOB_NAME, (job_name,))
+    cursor.execute(sql_query.FETCH_JOB_BY_JOB_NAME_QUERY, (job_name,))
 
     record = cursor.fetchone()
     return record
 
 def add_job(data):
-    cursor.execute(sql_query.INSERT_JOB, (data["job_name"], data["desc"], data["gaji"]))
+    cursor.execute(sql_query.INSERT_JOB_QUERY, (data["job_name"], data["desc"], data["gaji"]))
     
     connection.commit()
     return
 
 def add_application(data):
-    cursor.execute(sql_query.INSERT_APPLICATION, (data["user_id"], data["job_id"]))
+    cursor.execute(sql_query.INSERT_APPLICATION_QUERY, (data["user_id"], data["job_id"]))
 
     connection.commit()
     return
 
 def get_job_name(job_id):
-    cursor.execute(sql_query.FETCH_JOB_NAME_BY_JOB_ID, (job_id,))
-    
+    cursor.execute(sql_query.FETCH_JOB_NAME_BY_JOB_ID_QUERY, (job_id,))
+
     record = cursor.fetchone()
     return record
 
 def get_job_id():
-    cursor.execute(sql_query.FETCH_JOB_ID)
+    cursor.execute(sql_query.FETCH_JOB_ID_QUERY)
 
     record = cursor.fetchall()
     return record
--- src/repository/profile.py
+++ src/repository/profile.py
@@ -2,25 +2,25 @@ from apps.src.main import cursor, connection
 import sql_query
 
 def get_data_based_on_username(username):
-    cursor.execute(sql_query.FETCH_PROFILE_BY_USERNAME, (username,))
+    cursor.execute(sql_query.FETCH_PROFILE_BY_USERNAME_QUERY, (username,))
 
     record = cursor.fetchone()
     return  record
 
 def get_job(profile_id):
-    cursor.execute(sql_query.FETCH_JOB_BY_PROFILE_ID, (profile_id,))
+    cursor.execute(sql_query.FETCH_JOB_BY_PROFILE_ID_QUERY, (profile_id,))
 
     record = cursor.fetchall()
     return record
 
 def add_new_data(data):
-    cursor.execute(sql_query.INSERT_PROFILE, (data["name"], data["username"], data["password"]))
+    cursor.execute(sql_query.INSERT_PROFILE_QUERY, (data["name"], data["username"], data["password"]))
     
     connection.commit()
     return
 
 def get_user_id():
-    cursor.execute(sql_query.FETCH_ALL_USER_ID)
+    cursor.execute(sql_query.FETCH_ALL_USER_ID_QUERY)
 
     record = cursor.fetchall()
     return  record
