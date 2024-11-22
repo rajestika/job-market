656c39a9b29e311776009d93481b800c6e0c15c9
rajestika <rajestika@gmail.com>
2024-11-22 20:21:17 +0700

    replace query by sql query from sql_query.py

--- src/repository/profile.py
+++ src/repository/profile.py
@@ -1,45 +1,26 @@
 from apps.src.main import cursor, connection
+import sql_query
 
 def get_data_based_on_username(username):
-    cursor.execute("""SELECT
-                   p.id,
-                   p.name,
-                   p.username,
-                   p.password                 
-
-                   from profile p
-
-                   where p.username = %s""", (username,))
+    cursor.execute(sql_query.FETCH_PROFILE_BY_USERNAME, (username,))
 
     record = cursor.fetchone()
     return  record
 
 def get_job(profile_id):
-    cursor.execute("""SELECT
-                   j.name,
-                   j.description as desc,
-                   j.gaji
+    cursor.execute(sql_query.FETCH_JOB_BY_PROFILE_ID, (profile_id,))
 
-                   from job j
-
-                   join profile_job pj on j.id = pj.job_id
-
-                   where pj.profile_id = %s""", (profile_id,))
     record = cursor.fetchall()
     return record
 
 def add_new_data(data):
-    cursor.execute("INSERT INTO profile (name, username, password) VALUES (%s, %s, %s)", 
-            (data["name"], data["username"], data["password"]))
+    cursor.execute(sql_query.INSERT_PROFILE, (data["name"], data["username"], data["password"]))
+    
     connection.commit()
     return
 
 def get_user_id():
-    cursor.execute("""SELECT
-                   p.id
-                   
-                   from profile p
-                   """)
+    cursor.execute(sql_query.FETCH_ALL_USER_ID)
 
     record = cursor.fetchall()
     return  record
