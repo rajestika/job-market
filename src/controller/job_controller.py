5c9457f1634a11bd2408050b6cd9e07e2b5fd83a
rajestika <rajestika@gmail.com>
2024-11-24 14:44:14 +0700

    cek input path parameter null atau tidak

--- src/controller/job_controller.py
+++ src/controller/job_controller.py
@@ -2,6 +2,7 @@ from flask import request, Blueprint
 from apps.src.repository import job
 from apps.src.service import job_service
 from apps.src.enum.enum import JobStatus
+from apps.src.util.util import check_none_in_array
 
 job_blueprint = Blueprint("job_blueprint", __name__)
 
@@ -26,10 +27,20 @@ def apply_job():
 
 @job_blueprint.get("/jobs/<int:job_id>")
 def show_detail_job_id(job_id):
+    if (job_id is None):
+        return{
+            "message":"job_id should not be null"
+        }
+    
     response = job_service.job_details(job_id)
     return response
 
 @job_blueprint.get("/jobs/<int:job_id>/applicant/<int:applicant_id>/<JobStatus:job_status>")
 def review(job_id, applicant_id, job_status):
+    if(check_none_in_array([job_id, applicant_id, job_status])):
+        return{
+            "message":"job_id, applicant_id, or job_status should not be null"
+        }
+
     response = job_service.review_application(job_id, applicant_id, job_status)
     return response
